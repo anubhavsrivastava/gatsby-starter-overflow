36c6f14101c9f4e52f6c5adabab3e4f5bd269819
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-19 17:45:15 +0530

    added gatsby config

---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Overflow', // <title>
+  manifestName: 'Overflow',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-overflow/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Anubhav',
+  heading: 'Web Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+};
